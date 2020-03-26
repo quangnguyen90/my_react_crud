22ac4031fd88a662482ddaaeabfc810901886898
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-03-26 22:16:23 +0700

    update code

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux';
-import casts from './catReducer';
+import cats from './catReducer';
 
 const rootReducer = combineReducers({
     // short hand property names

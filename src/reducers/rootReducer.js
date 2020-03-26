64fab8a3a43d93ffda9ee544d2314015b1549cc7
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-03-26 23:30:43 +0700

    remove old version

--- src/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import cats from './catReducer';
-
-const rootReducer = combineReducers({
-    // short hand property names
-    cats
-});
-
-export default rootReducer;

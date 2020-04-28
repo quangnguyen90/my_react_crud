fd5347a13ca54428a46f012a0240ddcfe29c5231
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-04-29 03:36:33 +0700

    S5 - useState TodoForm

--- react-hooks-basic/src/App.js
+++ react-hooks-basic/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.scss';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -17,9 +18,21 @@ function App() {
     setTodoList(newTodoList);
   }
 
+  function handleTodoFormSubmit(formValues) {
+    // add new todo to current todo list
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues, // to get all value
+    }
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
       <h1>React Hook - TodoList</h1>
+      <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={hanldeTodoClick} />
     </div>
   );

ef3fda32d47eb5c9f58639c2035e013af813fa81
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-04-30 15:56:24 +0700

    Example Hobby using Redux

---
+++ frontend_hobby/main.js
@@ -0,0 +1,94 @@
+console.log(window.Redux);
+const { createStore } = window.Redux;
+
+// Setup redux store
+// state
+// reducer
+// store
+
+//----------------------------------------------------------
+
+// Step 1: Create initial state
+const initialSate = JSON.parse(localStorage.getItem('hobby_list')) || [];
+
+// Step 2: Create reducer
+const hobbyReducer = (state = initialSate, action) => {
+    //Redux-flow-4: reducer return updated state
+    switch (action.type) {
+        case 'ADD_HOBBY': {
+            const newList = [...state];
+            newList.push(action.payload);
+            return newList;
+        }
+        default:
+            return state;
+    }
+}
+
+// Step 3: Create store
+const store = createStore(hobbyReducer);
+
+//----------------------------------------------------------
+
+// Step 4: create function to get data from initial state
+// get redux hobby list
+const renderHobbyList = (hobbyList) => {
+    if (!Array.isArray(hobbyList) || hobbyList.length === 0) return;
+
+    const ulElement = document.querySelector('#hobbyListId');
+    if (!ulElement) return;
+
+    // reset previous content of ul
+    ulElement.innerHTML = '';
+
+    for (const hobby of hobbyList) {
+        const liElement = document.createElement('li');
+        liElement.textContent = hobby;
+
+        ulElement.appendChild(liElement);
+    }
+}
+
+//----------------------------------------------------------
+
+// Step 5: render data 
+// render Initial hobby list
+const initialHobbyList = store.getState();
+renderHobbyList(initialHobbyList);
+
+//----------------------------------------------------------
+
+// Step 6: create function to handle ACTION submit
+//Redux-flow-1: handle form submit
+const hobbyFormElement = document.querySelector('#hobbyForm');
+if (hobbyFormElement) {
+    const handleFormSubmit = (e) => {
+        // prevent browser form reloading
+        e.preventDefault();
+        const hobbyTextElement = hobbyFormElement.querySelector('#hobbyText');
+        if (!hobbyTextElement) return;
+        //Redux-flow-2: declare action with type & data 
+        const action = {
+            type: 'ADD_HOBBY',
+            payload: hobbyTextElement.value
+        }
+        //Redux-flow-3: dispatch action to store
+        store.dispatch(action);
+
+        //reset form
+        hobbyFormElement.reset();
+    }
+
+    hobbyFormElement.addEventListener('submit', handleFormSubmit);
+}
+
+//----------------------------------------------------------
+
+// Step 6: render updated list
+// Redux-flow-5: store return udpated state & render UI
+store.subscribe(() => {
+    const newHobbyList = store.getState();
+    renderHobbyList(newHobbyList);
+
+    localStorage.setItem('hobby_list', JSON.stringify(newHobbyList));
+});
